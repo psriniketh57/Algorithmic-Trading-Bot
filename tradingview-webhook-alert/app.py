572368f0964fec49b23b22709ff5362514a74868
psriniketh57 <66330346+psriniketh57@users.noreply.github.com>
2020-06-29 18:29:06 -0500

    Update app.py

--- tradingview-webhook-alert/app.py
+++ tradingview-webhook-alert/app.py
@@ -3,8 +3,8 @@ from chalice import Chalice
 
 app = Chalice(app_name='tradingview-webhook-alert')
 
-API_KEY = 'PKZAFNPXEIA6ULM2L4V3'
-SECRET_KEY = 'e3Rxik4NAta0uCqFrxjON9ZOOvfyJi34I3LwjBnX'
+API_KEY = '<YOUR API KEY HERE>'
+SECRET_KEY = '<YOUR API SECRET KEY HERE>'
 BASE_URL = "https://paper-api.alpaca.markets"
 ORDERS_URL = "{}/v2/orders".format(BASE_URL)
 HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
@@ -64,25 +64,3 @@ def sell_stock():
         'id': response['id'],
         'client_order_id': response['client_order_id']
     }
-
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
